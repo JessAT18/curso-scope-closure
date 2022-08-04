35be4177dd928c953db73227eb2422b94b116951
JessAT18 <j-aquino@outlook.com>
2022-08-04 11:59:32 -0400

    Hoisting

---
+++ src/3-hoisting/index.js
@@ -0,0 +1,12 @@
+// var nameOfDog;
+// console.log(nameOfDog);
+// var nameOfDog = 'Firulais';
+// console.log(nameOfDog);
+
+nameOfDog();
+
+function nameOfDog() {
+    console.log(`El mejor perrito es ${firulais}`);
+}
+
+var firulais = 'Firulais';
