0eda4d8a8f44593b7b9ce6c9af8d185fdf1286f5
JessAT18 <j-aquino@outlook.com>
2022-08-04 11:10:20 -0400

    use strict

---
+++ src/1-scope/strict.js
@@ -0,0 +1,13 @@
+//var pi = undefined;
+/*pi = 3.1416;
+console.log(pi);*/
+
+'use strict';
+pi = 3.1416;
+console.log(pi);
+
+function myFun() {
+    'use strict';
+    return pi = 3.1416;
+}
+console.log(myFun());
